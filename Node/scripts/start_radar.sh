f18cf376ec56df80c67d19a50e7a73ad60ad0584
chirp189@gmail.com <chirp189@gmail.com>
2026-02-05 10:05:00 -0800

    start_radar.sh should work now

--- Node/scripts/start_radar.sh
+++ Node/scripts/start_radar.sh
@@ -6,20 +6,23 @@ fi
 
 INTERFACE=$(nmcli device status | grep ethernet | awk '{print $1}' | head -n 1)
 # Suppress output of ip check
-if ! ip addr show "enP8p1s0" | grep -q "inet " > /dev/null 2>&1; then
+if ! ip addr show $INTERFACE | grep -q "inet " > /dev/null 2>&1; then
    sudo nmcli connection up $INTERFACE > /dev/null 2>&1 || { echo "Failed to bring up nmcli connection $INTERFACE"; exit 1; }
 fi
 
-pushd ../DCA1000/SourceCode/Release/ > /dev/null 2>&1 || { echo "Failed to cd to ../DCA1000/SourceCode/Release/"; exit 1; }
-sudo ./DCA1000EVM_CLI_Control fpga DCAconfig.json > /dev/null 2>&1 || { echo "Failed to configure FPGA"; exit 1; }
-sudo ./DCA1000EVM_CLI_Control record DCAconfig.json > /dev/null 2>&1 || { echo "Failed to configure record mode"; exit 1; }
-popd > /dev/null 2>&1
-
 pushd ../setup_radar/build/ > /dev/null 2>&1 || { echo "Failed to cd to ../setup_radar/build/"; exit 1; }
 # Background process, output redirected also
-nohup ./setup_radar > /dev/null 2>&1 < /dev/null &
+./setup_radar < /dev/null
+# nohup ./setup_radar > /dev/null 2>&1 < /dev/null &
+echo -e "\n------------------------------------ \nignore the core dumped error message\n------------------------------------"
 popd > /dev/null 2>&1
 
+pushd ../DCA1000/SourceCode/Release/ > /dev/null 2>&1 || { echo "Failed to cd to ../DCA1000/SourceCode/Release/"; exit 1; }
+sudo ./DCA1000EVM_CLI_Control fpga DCAconfig.json || { echo "Failed to configure FPGA"; exit 1; }
+sudo ./DCA1000EVM_CLI_Control record DCAconfig.json || { echo "Failed to configure record mode"; exit 1; }
+popd > /dev/null 2>&1
+
+
 pushd ../DCA1000/SourceCode/Release/ > /dev/null 2>&1 || { echo "Failed to cd to ../DCA1000/SourceCode/Release/ (2nd time)"; exit 1; }
-sudo ./DCA1000EVM_CLI_Control start_record DCAconfig.json -q > /dev/null 2>&1 || { echo "Failed to start_record"; exit 1; }
+sudo ./DCA1000EVM_CLI_Control start_record DCAconfig.json -q || { echo "Failed to start_record"; exit 1; }
 popd > /dev/null 2>&1

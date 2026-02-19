e9eefa2a51ff848c73e1afa5fac5d403a80fec12
Vihan Jayaraman <55373538+VihanJ@users.noreply.github.com>
2026-02-18 20:30:35 -0800

    untested scripts, including hardware trigger

--- Node/test/shim/run_nodes_and_calibration.sh
+++ Node/test/shim/run_nodes_and_calibration.sh
@@ -11,7 +11,7 @@ NODES=(
   "tien4:169.231.105.114"
 )
 
-SCRIPT_PATH="/Users/oseeniraj/Chirp-1/Node/test/shim/main-newp3d.py"
+SCRIPT_PATH="main-newp3d.py"
 NODE_USER="chirp"
 NODE_PASS="chirp"
 
@@ -24,12 +24,24 @@ for NODE in "${NODES[@]}"; do
   NODE_ID="${NODE%%:*}"
   IP="${NODE##*:}"
   echo -e "\n--- Connecting to $NODE_ID ($IP) ---"
-  sshpass -p "$NODE_PASS" ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no -o StrictHostKeyChecking=no -o ConnectTimeout=10 $NODE_USER@$IP \
-    "cd $GIT_REPO_PATH && git switch $GIT_BRANCH && git pull && export NODE_ID=$NODE_ID; echo $NODE_PASS | sudo -S bash Node/scripts/start_radar.sh && nohup python3 $SCRIPT_PATH > ${NODE_ID}_log.txt 2>&1 &" &
+  sshpass -p "$NODE_PASS" ssh -o StrictHostKeyChecking=no -o ConnectTimeout=10 $NODE_USER@$IP \
+    "cd $GIT_REPO_PATH && git switch $GIT_BRANCH && git pull && export NODE_ID=$NODE_ID; \
+        
+    cd Node/test/shim && \
+    pwd && \
+    nohup sudo ../../scripts/reset_radar.sh > ${NODE_ID}_reset_log.txt 2>&1 & \
+    nohup sudo ../../scripts/start_radar.sh > ${NODE_ID}_start_log.txt 2>&1 & \
+    nohup ./hardware_trigger/trigger_test > ${NODE_ID}_trigger_log.txt 2>&1 & \
+    
+    python3 -m venv .venv && \
+    . .venv/bin/activate && \
+    pwd
+    pip install -r reqs.txt && \
+    nohup python3 $SCRIPT_PATH > ${NODE_ID}_log.txt 2>&1 &" & \
 done
 
 # Wait a few seconds for nodes to start
 sleep 5
 
 echo -e "\n--- Starting real-time spatial calibration on this machine ---"
-python3 /Users/oseeniraj/Chirp-1/Node/test/shim/spatial_calibration.py
+python3 spatial_calibration.py

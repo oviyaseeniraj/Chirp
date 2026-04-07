5dd9abf64d7f647776a1d60e0da92c471a582a9a
Chirp <chirp189@gmail.com>
2026-04-07 09:58:43 -0700

    organizational stuff

--- Node/src/hardware_trigger/config.h
+++ Node/src/hardware_trigger/config.h
@@ -9,6 +9,7 @@
 #define PULSE_PERIOD 100  // Period in milliseconds
 
 // Networking (used by networked_trigger.c)
+// Note: these are hardcoded values, but eventually we want to switch to environment variables that are resolved at runtime
 #define MASTER_IP "169.231.38.239"
 #define MASTER_PORT 1210
 #define SYNC_INTERVAL_SEC 5

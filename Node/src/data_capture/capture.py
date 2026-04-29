c4a412955f7b48f529fa8a384b8722438faf97bc
Chirp <chirp189@gmail.com>
2026-04-29 14:01:10 -0700

    manually bringing in pipeline changes from Will into this branch -- this is now our most current working version

--- Node/src/data_capture/capture.py
+++ Node/src/data_capture/capture.py
@@ -35,7 +35,8 @@ class CaptureSession:
         """
         self.logger.info(f"Starting capture of {num_frames} frames...")
         
-        for i in range(num_frames):
+        i = 0
+        while i < num_frames:
             try:
                 # 1. Capture Raw Data
                 # daq.capture() returns a uint16 numpy array
@@ -44,7 +45,8 @@ class CaptureSession:
                 except TimeoutError as te:
                     self.logger.error(f"Timeout capturing frame {i}: {te}")
                     continue
-
+                
+                i += 1
                 # 2. Save Raw Data
                 if save_raw:
                     raw_filename = os.path.join(self.raw_dir, f"raw_frame_{i:04d}.npy")

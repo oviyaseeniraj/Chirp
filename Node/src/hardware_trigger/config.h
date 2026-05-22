c7a980f5caa1b44fd14ceef1f90c4fcebb44dddd
Andrey <andreyotvagingrey@gmail.com>
2026-05-22 13:45:06 -0700

    more speed

--- Node/src/hardware_trigger/config.h
+++ Node/src/hardware_trigger/config.h
@@ -6,7 +6,7 @@
 #define PULSE_WIDTH_NS 50
 
 // Trigger Timing
-#define PULSE_PERIOD 100  // Period in milliseconds
+#define PULSE_PERIOD 65  // Period in milliseconds
 
 // Networking (used by networked_trigger.c)
 // Note: these are hardcoded values, but eventually we want to switch to environment variables that are resolved at runtime
